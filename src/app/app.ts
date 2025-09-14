908e331ef7d8c8520705f024bb1b6dafb7e7cbfa
Fahad almutairi <441023372@sm.imamu.edu.sa>
2025-09-14 20:47:45 +0300

    todo list app save onto array

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,44 @@
+import { NgClass } from '@angular/common';
 import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
+export interface TodoItem{
+  id:number;
+  task:string;
+  completed:boolean;
+
+}
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, FormsModule, NgClass],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('TODO-app');
+
+todolist: TodoItem[] = [];
+newTask : string = '';
+
+addTask(): void{
+  if(this.newTask.trim() !== ''){
+    const newTodoItem : TodoItem = {
+      id : Date.now(),
+      task : this.newTask,
+      completed : false ,
+    }
+    this.todolist.push(newTodoItem);
+    this.newTask = '';
+  }
+}
+completeTask(todoitem : TodoItem) : void {
+todoitem.completed = true ;
+}
+
+toggleCompleted(index: number){
+  this.todolist[index].completed = !this.todolist[index].completed;
+}
+DeleteTask(id: number){
+  this.todolist = this.todolist.filter(item => item.id !== id)
+}
 }
